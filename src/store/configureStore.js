fda2a4c6392b2c8754999c0bb29fed67e8faeaa5
Louis <delwiv@protonmail.com>
2022-01-14 11:35:52 +0100

    feat: rm performance

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,7 +2,7 @@ import { applyMiddleware, createStore } from 'redux'
 import thunkMiddleware from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 
-import monitorReducersEnhancer from './enhancers/monitorReducers'
+// import monitorReducersEnhancer from './enhancers/monitorReducers'
 import loggerMiddleware from './middleware/logger'
 import rootReducer from './reducers/index.js'
 
@@ -10,7 +10,7 @@ export default function configureStore(preloadedState) {
   const middlewares = [loggerMiddleware, thunkMiddleware]
   const middlewareEnhancer = applyMiddleware(...middlewares)
 
-  const enhancers = [middlewareEnhancer, monitorReducersEnhancer]
+  const enhancers = [middlewareEnhancer]
   const composedEnhancers = composeWithDevTools(...enhancers)
 
   const store = createStore(rootReducer, preloadedState, composedEnhancers)
